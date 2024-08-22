858114781d1e5ba0a987f0d83343daf8e7d9afb0
Sean Kang <cbkang@att.net>
2024-08-22 15:43:38 -0400

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+
+import '../styles/global.css';
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+}
---
+++ pages/api/hello.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+
+export default function handler(req, res) {
+    res.status(200).json({ text: 'Hello' });
+}
